64ab3dd07c96a8e657f32bd8fc44f80b2e96450b
amansingh2505 <amansinghrajput2352@gmail.com>
2026-06-12 19:12:42 +0530

    gitpage2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/Mini-Anveshana_2025-26/' : '/',
+  base: mode === 'production' ? '/Annavaani/' : '/',
   server: {
     port: 5173,
     proxy: {
